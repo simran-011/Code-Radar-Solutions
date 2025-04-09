694354d0e531bf12ae5fc95fd1a9ee21cc4e3094
simran-011 <simrananand910@gmail.com>
2025-04-09 12:01:08 +0530

    Solution to Caesar Cipher Encryption

--- Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
+++ Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
@@ -1,6 +1,6 @@
 #include <string.h>
-void caesarCipher(char message[], int shift,  char encrypted[]) {
-    // char encrypted[100];
+void caesarCipher(char message[], int shift,  ) {
+    char encrypted[100];
     for(int i=0; i< strlen(message); i++) {
         if((message[i] >=65 && message[i]<=90) || message[i]>=97 && message[i] <=122) {
                encrypted[i] = message[i] + shift;
